a6ea13f434c2aef191b0d8c521c3ceef1fc329b0
Gary Spagnoli <gary.spagnoli@gmail.com>
2014-05-27 16:55:51 -0400

    Adding some rspec testing and the todo list items to finally work properly

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module Odot
   class Application < Rails::Application
@@ -19,5 +19,6 @@ module Odot
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    I18n.enforce_available_locales = false
   end
 end
